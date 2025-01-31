1a009712e8d0c1d48ec7d7af7f36db0b2293272b
Jaider ponce <jaiderponce@gmail.com>
2025-01-31 16:13:24 -0400

    modificando nombre de base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
   //base: '/tareas/'
-  base:'https://jaiderpobce.github.io/tarea/'
+  base:'https://jaiderpobce.github.io/tareas/'
 })
